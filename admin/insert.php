5c66f396dd1aaaef93e6de02d5d9136d58620f3e
n67h <night67hunter@gmail.com>
2023-03-24 10:48:53 +0800

    changed some variable names

--- admin/insert.php
+++ admin/insert.php
@@ -5,11 +5,11 @@
     $name = 'Andre Paul N. Sta. Clara';
     $email = 'andrepaul.staclara67@gmail.com';
     $password = 'qweqweqwe';
-    $contact = '09123456789';
+    $contact = '09298410728';
     
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO new_trend_user (user_level, name, email, password, contact) VALUES ('$level', '$name', '$email', '$hashed_password', '$contact');";
+    $sql = "INSERT INTO new_trend_user (user_level, name, email, password, contact) VALUES ('$user_level', '$name', '$email', '$hashed_password', '$contact');";
 
     if(mysqli_query($conn, $sql)){
         echo 'success';
