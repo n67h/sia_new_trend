272879f0c8b585b1fcfc0f52a8112cc9e93aa663
n67h <night67hunter@gmail.com>
2023-04-02 22:51:10 +0800

    Fix edit category
    Replace service.php to category.php on line 18

--- admin/includes/edit-category.inc.php
+++ admin/includes/edit-category.inc.php
@@ -15,7 +15,7 @@
                 function countdown() {
                     var i = document.getElementById("counter");
                     if (parseInt(i.innerHTML)<=0) {
-                        location.href = "../service.php";
+                        location.href = "../category.php";
                     }
                     i.innerHTML = parseInt(i.innerHTML)-1;
                 }
